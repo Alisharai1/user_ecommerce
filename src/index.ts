c6e6b1728cd043897297aea877a94287135ce2b7
Alisharai1 <alisharai1997@gmail.com>
2026-09-20 00:33:14 +0530

    nodemon,dotenv setup done

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express"
 import { Request, Response } from "express"
 import { bootstrap } from "./bootstrap"
+import 'dotenv/config'
+import { Database } from "./repo/db"
+
+export const db = Database.Connection({
+    dbName: process.env.DB_NAME!,
+    userName: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    host: process.env.DB_HOST!,
+})
 
 const app = express()
 
-const port = 3000
+const port = Number(process.env.PORT) || 4000;
+
 
 app.use(express.json())
 
--- src/repo/db.ts
+++ src/repo/db.ts
@@ -1,28 +1,26 @@
 import { Sequelize } from "sequelize";
 
- export class Database{
+export class Database {
 
     static Connection(input: {
-    "dbName": string,
-    "userName": string,
-    "password": string,
-}) {
-    const db = new Sequelize(input.dbName, input.userName, input.password, {
-        host: 'localhost',
-        dialect: "postgres"
-    })
-    return db
-}
+        "dbName": string,
+        "userName": string,
+        "password": string,
+        "host": string
+    }) {
+        const db = new Sequelize(input.dbName, input.userName, input.password, {
+            host: 'localhost',
+            dialect: "postgres"
+        })
+        return db
+    }
 
- static async  TestConnection(db: Sequelize) {
-    try {
-        await db.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+    static async TestConnection(db: Sequelize) {
+        try {
+            await db.authenticate();
+            console.log('Connection has been established successfully.');
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
     }
 }
-
- }
-
-
